b42b9c5a7700bce065c33bb0d1ee12984ee4c0a2
shane <psh40963@naver.com>
2024-11-07 21:33:09 +0900

    test: add simple test code, support windows

--- src/test/java/shanepark/foodbox/image/ImageParserTest.java
+++ src/test/java/shanepark/foodbox/image/ImageParserTest.java
@@ -1,7 +1,9 @@
 package shanepark.foodbox.image;
 
+import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,30 +20,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ImageParserTest {
     private final String TESSDATA_PATH_LINUX = "/usr/share/tesseract-ocr/5/tessdata";
     private final String TESSDATA_PATH_MAC = "/opt/homebrew/share/tessdata";
+    private final String TESSDATA_PATH_WIN = "C:\\Program Files\\Tesseract-OCR\\tessdata";
     private final Logger logger = LoggerFactory.getLogger(ImageParserTest.class);
 
     ImageParser parser;
 
     @BeforeEach
     void setUp() {
-        String os = System.getProperty("os.name").toLowerCase();
-        String dataPath;
-        if (os.contains("mac")) {
-            dataPath = TESSDATA_PATH_MAC;
-        } else {
-            dataPath = TESSDATA_PATH_LINUX;
-        }
+        String dataPath = getDataPathFromOs();
         parser = new ImageParser(dataPath);
     }
 
+    private String getDataPathFromOs() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("mac")) {
+            return TESSDATA_PATH_MAC;
+        }
+        if (os.contains("win")) {
+            return TESSDATA_PATH_WIN;
+        }
+        if (os.contains("linux")) {
+            return TESSDATA_PATH_LINUX;
+        }
+        throw new RuntimeException("Unsupported OS");
+    }
+
     @Test
+    @DisplayName("Parse image and return parsed menu")
     void parse() throws IOException, TesseractException {
         ClassPathResource resource = new ClassPathResource("메뉴.png");
         File file = resource.getFile();
 
         List<ParsedMenu> parsedMenu = parser.parse(file);
-        logger.info("parsedMenu: {}", parsedMenu);
+        logger.info("parsedMenu: \n{}", parsedMenu);
         assertThat(parsedMenu).hasSize(10);
         assertThat(parsedMenu.getFirst().getDate()).isEqualTo("10/28(월)");
     }
+
+    @Test
+    @DisplayName("Simply parse image just as text")
+    void simpleParse() throws IOException {
+        ClassPathResource resource = new ClassPathResource("메뉴.png");
+        File file = resource.getFile();
+
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath(getDataPathFromOs());
+        tesseract.setLanguage("kor");
+
+        try {
+            String result = tesseract.doOCR(file);
+            logger.info("result: \n{}", result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

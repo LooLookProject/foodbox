a3da6a23bba852e5fa9d19d1aed18270304cc992
shane <psh40963@naver.com>
2024-11-17 00:41:27 +0900

    chores: ignore favicon warning

--- src/main/java/shanepark/foodbox/api/controller/ErrorControllerAdvice.java
+++ src/main/java/shanepark/foodbox/api/controller/ErrorControllerAdvice.java
@@ -3,6 +3,7 @@ package shanepark.foodbox.api.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import shanepark.foodbox.api.domain.ApiResponse;
 
 @RestControllerAdvice
@@ -11,6 +12,11 @@ public class ErrorControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ApiResponse handleException(Exception e) {
+        if (e instanceof NoResourceFoundException) {
+            // ignore favicon error
+            return null;
+        }
+        
         log.error("Error occurred", e);
         return ApiResponse.fail(e);
     }
